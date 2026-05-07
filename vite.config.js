95f733f0aed712f6cf0f67fd172099358fc837d9
HIOJO <31084144+HIOJO@users.noreply.github.com>
2026-05-07 19:34:39 -0300

    Add files via upload
    SaaS-veo-3

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://136.248.93.175:8000',
+        changeOrigin: true
+      }
+    }
+  }
+})
